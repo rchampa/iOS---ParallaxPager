c209a18bca64fff9fd41c92344885d6a0030785c
rchampa <ricardo.champa@spamina.com>
2016-10-14 15:24:07 +0200

    first commit

---
+++ NDParallaxIntroViewExample/NDParallaxIntroViewExample/ViewController.m
@@ -0,0 +1,82 @@
+//
+//  ViewController.m
+//  NDParallaxIntroViewExample
+//
+//  Created by Simon Wicha on 17/04/2016.
+//  Copyright © 2016 Simon Wicha. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NDIntroView.h"
+
+@interface ViewController () <NDIntroViewDelegate>
+
+@property (strong, nonatomic) NDIntroView *introView;
+
+@property (weak, nonatomic) IBOutlet UIButton *restartButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.restartButton setBackgroundColor:[UIColor colorWithRed:70.f/255.f green:130.f/255.f blue:180.f/255.f alpha:1.f]];
+    self.restartButton.layer.cornerRadius = 5.f;
+    
+    [self startIntro];
+}
+
+- (IBAction)restartIntroViewButtonPressed:(id)sender {
+    [self startIntro];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - NDIntroView methods
+
+-(void)startIntro {
+    NSArray *pageContentArray = @[@{kNDIntroPageTitle : @"NDParallaxIntroView",
+                                    kNDIntroPageDescription : @"Now you can easily add your beautiful intro into your app with no hassle.",
+                                    kNDIntroPageImageName : @"parallax",
+                                    kNDShowInputUser : @NO,
+                                    },
+                                  @{kNDIntroPageTitle : @"Work-It-Out",
+                                    kNDIntroPageDescription : @"A great App to create your own personal workout and get instructed by your phone.",
+                                    kNDIntroPageImageName : @"workitout",
+                                    kNDShowInputUser : @YES,
+                                    },
+                                  @{kNDIntroPageTitle : @"ColorSkill",
+                                    kNDIntroPageDescription : @"A small game while waiting for the bus. Easy, quick and addictive.",
+                                    kNDIntroPageImageName : @"colorskill",
+                                    kNDShowInputUser : @NO,
+                                    },
+//                                  @{kNDIntroPageTitle : @"Appreciate",
+//                                    kNDIntroPageDescription : @"A little helper to make your life happier. Soon available on the AppStore",
+//                                    kNDIntroPageImageName : @"appreciate",
+//                                    kNDShowInputUser : @NO,
+//                                    },
+//                                  @{kNDIntroPageTitle : @"Do you like it?",
+//                                    kNDIntroPageImageName : @"firstImage",
+//                                    kNDIntroPageTitleLabelHeightConstraintValue : @0,
+//                                    kNDIntroPageImageHorizontalConstraintValue : @-40,
+//                                    kNDShowInputUser : @NO,
+//                                    }
+                                  ];
+    self.introView = [[NDIntroView alloc] initWithFrame:self.view.frame parallaxImage:[UIImage imageNamed:@"parallaxBgImage"] andData:pageContentArray];
+    self.introView.delegate = self;
+    [self.view addSubview:self.introView];
+}
+
+-(void)launchAppButtonPressed {
+    [UIView animateWithDuration:0.7f animations:^{
+        self.introView.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self.introView removeFromSuperview];
+    }];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,66 @@
+# NDParallaxIntroView
+
+A very easy to use and beautiful parallax IntroView for your App to onboard user.
+
+## Installation
+
+Just copy the NDParallaxIntroView folder into your project. The pod version is available on https://github.com/s1m-0n/NDParallaxIntroView-Pod
+
+## Usage
+
+Create a strong property of NDParallaxIntroView in your class from which you want to trigger your intro.
+``` objective-c
+#import "NDParallaxIntroView.h"
+
+@property (strong, nonatomic) NDParallaxIntroView *introView;
+
+@interface ViewController : UIViewController <NDParallaxIntroViewDelegate>
+```
+Usage:
+``` objective-c
+self.introView = [[NDIntroView alloc] initWithFrame:self.view.frame parallaxImage:[UIImage imageNamed:@"parallaxBgImage"] andData:pageContentArray];
+self.introView.delegate = self;
+[self.view addSubview:self.introView];
+```
+Important - You need to create the pageContentArray first which follows an easy structure (see example below). All values are optional so don't worry about the app crashing when you forget something ;)
+``` objective-c
+NSArray *pageContentArray = @[@{kNDIntroPageTitle : @"Work-It-Out",
+                                    kNDIntroPageDescription : @"A great App to create your own personal workout and get instructed by your phone.",
+                                    kNDIntroPageImageName : @"workitout"
+                                    },
+                                  @{kNDIntroPageTitle : @"ColorSkill",
+                                    kNDIntroPageDescription : @"A small game while waiting for the bus. Easy, quick and addictive.",
+                                    kNDIntroPageImageName : @"colorskill"
+                                    },
+                                  @{kNDIntroPageTitle : @"Appreciate",
+                                    kNDIntroPageDescription : @"A little helper to make your life happier. Soon available on the AppStore",
+                                    kNDIntroPageImageName : @"appreciate"
+                                    },
+                                  @{kNDIntroPageTitle : @"Do you like it?",
+                                    kNDIntroPageImageName : @"firstImage",
+                                    kNDIntroPageTitleLabelHeightConstraintValue : @0,
+                                    kNDIntroPageImageHorizontalConstraintValue : @-40
+                                    }
+                                  ];
+```
+Delegate method implementation is required
+``` objective-c
+-(void)launchAppButtonPressed
+```
+
+## Screenshot
+
+Animated gif of NDParallaxIntroView is action
+
+![](https://github.com/s1m-0n/NDParallaxIntroView/blob/master/NDParallaxIntroViewExample/ndparallaxintroview.gif "NDParallaxIntroView")
+
+
+
+## Author
+
+Simon Wicha | simon.wicha@nomis-development.net
+
+## License
+
+NDParallaxIntroView is available under the MIT license. See the LICENSE file for more info.
+
